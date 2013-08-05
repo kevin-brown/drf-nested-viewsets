28088527853ee479876925904fd400aa8cbb707d
Kevin Brown <bass5098@gmail.com>
2013-08-05 10:09:18 -0400

    Added initial work for the router and fields

---
+++ rest_nested_viewsets/fields.py
@@ -0,0 +1,52 @@
+from rest_framework.serializers import HyperlinkedIdentityField, \
+HyperlinkedRelatedField
+
+
+class NestedHyperlinkedField(object):
+    
+    base_obj = None
+    
+    def __init__(self, *args, **kwargs):
+        self.map = kwargs.pop("map", {})
+        
+        super(NestedHyperlinkedField, self).__init__(*args, **kwargs)
+    
+    def field_to_native(self, obj, field_name):
+        self.base_obj = obj
+        
+        return super(NestedHyperlinkedField, self).field_to_native(obj,
+                                                                   field_name)
+    
+    def get_url(self, obj, view_name, request, format):
+        from django.core.urlresolvers import NoReverseMatch
+        from rest_framework.reverse import reverse
+        
+        try:
+            return super(NestedHyperlinkedField, self).get_url(obj,
+                view_name, request, format)
+        except NoReverseMatch:
+            pass
+        
+        kwargs = {
+            "pk": obj.pk,
+        }
+        
+        serializer = self.parent
+        view = serializer.context["view"]
+        
+        for key, kwkey in self.map.iteritems():
+            base_obj = self.base_obj.pk if self.base_obj else 0
+            kwargs[key] = view.kwargs.get(kwkey, base_obj)
+        
+        url = reverse(view_name, kwargs=kwargs, request=request, format=format)
+        
+        return url
+
+
+class HyperlinkedIdentityField(NestedHyperlinkedField,
+                               HyperlinkedIdentityField):
+    pass
+
+
+class HyperlinkedRelatedField(NestedHyperlinkedField, HyperlinkedRelatedField):
+    pass
---
+++ rest_nested_viewsets/routers.py
@@ -0,0 +1,33 @@
+from rest_framework import routers
+
+
+class NestedRouter(routers.DefaultRouter):
+    
+    def get_api_root_view(self):
+        """
+        Return a view to use as the API root.
+        """
+        
+        from django.core.urlresolvers import NoReverseMatch
+        from rest_framework import views
+        from rest_framework.response import Response
+        from rest_framework.urlresolvers import reverse
+        
+        api_root_dict = {}
+        list_name = self.routes[0].name
+        for prefix, viewset, basename in self.registry:
+            api_root_dict[prefix] = list_name.format(basename=basename)
+ 
+        class APIRoot(views.APIView):
+            _ignore_model_permissions = True
+ 
+            def get(self, request, format=None):
+                ret = {}
+                for key, url_name in api_root_dict.items():
+                    try:
+                        ret[key] = reverse(url_name, request=request, format=format)
+                    except NoReverseMatch:
+                        pass
+                return Response(ret)
+ 
+        return APIRoot.as_view()
